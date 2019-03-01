193c5fdc1ab6179ae1aed4d7c5bcec7ed58c8752
peterhchen <peter.hp.chen@gmail.com>
2019-02-28 21:46:33 -0800

    File System

---
+++ ch12_file/ch1207_DeleteFile.js
@@ -0,0 +1,10 @@
+//comment
+var fs = require("fs");
+
+console.log("Going to delete an existing file");
+fs.unlink('input.txt', function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("File deleted successfully!");
+});
